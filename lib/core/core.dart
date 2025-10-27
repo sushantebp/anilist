9fd48d2e5c1521fd288ae823df7de06f6269bf03
sushant <sushant.khadka@ebpearls.com>
2025-10-27 13:57:30 +0545

    added static appliance for use

--- lib/core/core.dart
+++ lib/core/core.dart
@@ -1,3 +1,5 @@
 export './config/app_config.dart';
 export './di/di.dart';
 export './router/app_router.dart';
+export './constants/app_size.dart';
+export './extensions/build_context_extension.dart';
